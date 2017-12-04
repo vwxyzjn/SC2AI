0a1beb944bc0fec6762ea9d65100ddafaf542623
Costa Huang <costa.huang@outlook.com>
2017-12-04 00:03:03 -0500

    Add random agent for comparison

--- micro/random_agent/random_agent.py
+++ micro/random_agent/random_agent.py
@@ -15,7 +15,7 @@
 
 import numpy as np
 
-from tensorforce.agents import PPOAgent
+from tensorforce.agents import RandomAgent
 from tensorforce.execution import Runner
 from tensorforce.contrib.openai_gym import OpenAIGym
 
@@ -26,63 +26,13 @@ FLAGS([__file__])
 
 
 # Create an OpenAIgym environment
-# ReversedAddition-v0
-# CartPole-v0
 env = OpenAIGym('SC2CollectMineralShards-v2', visualize=False)
 
-# Network as list of layers
-network_spec = [
-    dict(type='conv2d', size=64),
-    dict(type='flatten'),
-    dict(type='dense', size=32, activation='relu'),
-    dict(type='lstm', size=64)
-]
 
-saver_spec = {
-#    'load': True,
-#    'file': 'model.ckpt-7914479',
-    'directory': './model',
-    'seconds': 3600
-}
-
-agent = PPOAgent(
+agent = RandomAgent(
     states_spec=env.states,
     actions_spec=env.actions,
-    network_spec=network_spec,
-    batch_size=10,
-    # Agent
-    preprocessing=None,
-    exploration=None,
-    reward_preprocessing=None,
-    saver_spec=saver_spec,
-    # BatchAgent
-    keep_last_timestep=True,
-    # PPOAgent
-    step_optimizer=dict(
-        type='adam',
-        learning_rate=5e-6
-    ),
-    optimization_steps=10,
-    # Model
-    scope='ppo',
-    discount=0.99,
-    # DistributionModel
-    distributions_spec=None,
-    entropy_regularization=0.01,
-    # PGModel
-    baseline_mode=None,
-    baseline=None,
-    baseline_optimizer=None,
-    gae_lambda=0.99,
-    normalize_rewards=False,
-    # PGLRModel
-    likelihood_ratio_clipping=0.2,
-    summary_spec=None,
-    distributed_spec=None
 )
-    
-print('partially success')
-
 # Create the runner
 runner = Runner(agent=agent, environment=env)
 
@@ -98,7 +48,7 @@ def episode_finished(r):
 
 
 # Start learning
-runner.run(episodes=60000, episode_finished=episode_finished)
+runner.run(episodes=200, episode_finished=episode_finished)
 
 # Print statistics
 print("Learning finished. Total episodes: {ep}. Average reward of last 100 episodes: {ar}.".format(
